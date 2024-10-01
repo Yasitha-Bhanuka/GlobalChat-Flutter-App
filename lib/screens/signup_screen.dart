683c2f5fca879d417b3f7d8eec063ce95c93c715
yasithabhanuka <yasithabhanukac@gmail.com>
2024-10-01 17:58:50 +0530

    Enable the scrolling on the TextFormFields if the keyboard is pop up in the signup screen using SingleChildScrollView.

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -22,106 +22,117 @@ class _SignupScreenState extends State<SignupScreen> {
       appBar: AppBar(
         title: Text(""),
       ),
-      body: Form(
-        key: userForm,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                  height: 150,
-                  width: 150,
-                  child: Image.asset("assets/images/logo.png")),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                controller: emailController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Name is required";
-                  }
-                },
-                decoration: InputDecoration(
-                  label: Text("Name"),
-                ),
-              ),
-              SizedBox(
-                height: 23,
-              ),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                controller: emailController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Country is required";
-                  }
-                },
-                decoration: InputDecoration(
-                  label: Text("Country"),
-                ),
-              ),
-              SizedBox(
-                height: 23,
-              ),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                controller: emailController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Email is required";
-                  }
-                },
-                decoration: InputDecoration(
-                  label: Text("Email"),
-                ),
-              ),
-              SizedBox(
-                height: 23,
-              ),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                controller: passwordController,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Password is required";
-                  }
-                },
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  label: Text("Password"),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            minimumSize: Size(0, 50),
-                            foregroundColor: Colors.white,
-                            backgroundColor: Color.fromARGB(255, 10, 194, 236)),
-                        onPressed: () {
-                          if (userForm.currentState!.validate()) {
-                            // create account
-                            SignupController.createAccount(
-                                context: context,
-                                emailController: emailController.text,
-                                passwordController: passwordController.text,
-                                nameController: nameController.text,
-                                countryController: countryController.text);
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Form(
+                key: userForm,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: Image.asset("assets/images/logo.png")),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Name is required";
                           }
                         },
-                        child: Text("Create an account")),
+                        decoration: InputDecoration(
+                          label: Text("Name"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 23,
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Country is required";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          label: Text("Country"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 23,
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Email is required";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          label: Text("Email"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 23,
+                      ),
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Password is required";
+                          }
+                        },
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                          label: Text("Password"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(0, 50),
+                                    foregroundColor: Colors.white,
+                                    backgroundColor:
+                                        Color.fromARGB(255, 10, 194, 236)),
+                                onPressed: () {
+                                  if (userForm.currentState!.validate()) {
+                                    // create account
+                                    SignupController.createAccount(
+                                        context: context,
+                                        emailController: emailController.text,
+                                        passwordController:
+                                            passwordController.text,
+                                        nameController: nameController.text,
+                                        countryController:
+                                            countryController.text);
+                                  }
+                                },
+                                child: Text("Create an account")),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
