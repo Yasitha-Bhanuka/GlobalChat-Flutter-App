4467dde59e5ced7e5bde02c323e8c6e672186cdc
yasithabhanuka <yasithabhanukac@gmail.com>
2024-10-01 08:15:46 +0530

    Update the App Icon And App Name for IOS and Android Devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:globalchat/firebase_options.dart';
 
 Future<void> main() async {
